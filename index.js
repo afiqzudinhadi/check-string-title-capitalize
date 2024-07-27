888243d07bff75e925c429d640a9115377a83ed2
afiqzudinhadi <afiqzudinhadi@gmail.com>
2024-07-28 04:18:03 +0800

    feat: add logic

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import { input } from '@inquirer/prompts';
 
-const answer = await input({ message: 'Enter your name' });
+const answer = await input({ message: 'Enter your string:' });
+
+const words = answer.split(' ')
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i][0] !== words[i][0].toUpperCase()) {
+        console.log(false)
+        break
+    }
+
+    if (i === words.length - 1) {
+        console.log(true)
+    }
+}
